ef6533a768207e81063df5fbd479eec6a27b0cd9
Michal Ogorek <michal.ogorek03@gmail.com>
2025-12-27 13:57:43 +0100

    created dto for adding new campaign

---
+++ src/main/java/org/example/futurumtechnologyrecruitmenttask/dto/AddNewCampaignRequest.java
@@ -0,0 +1,35 @@
+package org.example.futurumtechnologyrecruitmenttask.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record AddNewCampaignRequest(
+               @NotBlank(message="Campaign name can not be null or empty.")
+               String name,
+
+               @NotEmpty(message="Keywords field is mandatory.")
+               List< @NotBlank(message = "Keyword can not be null or empty.")String> keywords,
+
+               @NotNull(message="Bid amount field is mandatory.")
+               @Min(value=0, message="Bid amount value must be higher or equal 0.")
+               Double bidAmount,
+
+               @NotNull(message="Campaign fund field is mandatory.")
+               @Min(value=0, message="Campaign fund value must be higher or equal 0.")
+               Double fund,
+
+               @NotNull(message="Status field is mandatory.")
+               Boolean status,
+
+               @NotBlank(message="Town field can not be null or empty.")
+               String town,
+
+               @NotNull(message="Radius field is mandatory.")
+               //@Min(value=0,message="Radius value must be higher or equal 0.")
+               Double radius
+) {
+}
