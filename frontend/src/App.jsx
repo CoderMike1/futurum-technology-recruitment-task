6c930d2b58374c1428db3bbc3c68ae91d952e63d
Michal Ogorek <michal.ogorek03@gmail.com>
2025-12-27 17:51:15 +0100

    added UI for managing data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import {useEffect, useState} from "react";
+import AddRecordForm from "./forms/AddRecordForm.jsx";
+import EditRecordForm from "./forms/EditRecordForm.jsx";
 
 const URL = "http://localhost:8080/api/campaigns"
 
@@ -9,64 +11,105 @@ function App() {
 
     const [loading,setLoading] = useState(true);
     const [errorMessage,setErrorMessage] = useState(null);
+    const [adding,setAdding] = useState(false)
+    const [editing,setEditing] = useState(false)
+
+    const [selectedRecord,setSelectedRecord] = useState(null)
 
 
+
+    const loadData = async ()=>{
+        const resp = await fetch(URL, {
+            method:"GET"
+        })
+        if (!resp.ok){
+            setErrorMessage("Error while loading the data.")
+        }
+        else{
+            const data = await resp.json()
+            setCampaignsList(data)
+        }
+    }
+
+    const deleteRecord = async (id) =>{
+        setErrorMessage(null)
+        const resp = await fetch(`${URL}/delete/${id}`,{
+            method:"DELETE"
+        })
+        if(resp.status !== 204){
+            setErrorMessage("Error while deleting record.")
+        }
+        else{
+            await loadData()
+        }
+
+    }
+
+    const handleEdit = (c)=>{
+        setEditing(true)
+        setSelectedRecord(c)
+    }
 
     useEffect(() => {
 
         (async ()=>{
-
-            const resp = await fetch(URL, {
-                method:"GET"
-            })
-            if (!resp.ok){
-                setErrorMessage("Error while loading the data.")
-            }
-            else{
-                const data = await resp.json()
-                setCampaignsList(data)
-            }
+            await loadData()
+            setLoading(false)
         })()
 
-    }, []);
+    }, [adding,editing]);
+
+
+
 
 
   return (
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Keywords</th>
-                    <th>Bid amount</th>
-                    <th>Campaign fund</th>
-                    <th>Status</th>
-                    <th>Town</th>
-                    <th>Radius</th>
-                </tr>
+      <div className="main-container">
+          {editing && <EditRecordForm selectedRecord={selectedRecord} setEditing={setEditing}/>}
+          {adding && <AddRecordForm setAdding={setAdding}/>}
+          <div className="table-container">
+              <p className="loading-field">{loading && "Loading..."}</p>
+              <p className="error-field">{errorMessage}</p>
+              <table>
+                  <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Keywords</th>
+                      <th>Bid amount</th>
+                      <th>Campaign fund</th>
+                      <th>Status</th>
+                      <th>Town</th>
+                      <th>Radius</th>
+                      <th>Actions</th>
+                  </tr>
 
-            </thead>
-            <tbody>
-                {campaignsList.map((campaign)=>(
-                    <tr key={campaign.id}>
-                        <td>{campaign.id}</td>
-                        <td>{campaign.name}</td>
-                        <td>{campaign.keywords.join(",")}</td>
-                        <td>{campaign.bidAmount}</td>
-                        <td>{campaign.campaignFund}</td>
-                        <td>{campaign.status}</td>
-                        <td>{campaign.town}</td>
-                        <td>{campaign.radius}</td>
-                    </tr>
-                ))}
-            </tbody>
+                  </thead>
+                  <tbody>
+                  {campaignsList.map((campaign)=>(
+                      <tr key={campaign.id} className="table-item">
+                          <td>{campaign.id}</td>
+                          <td>{campaign.name}</td>
+                          <td>{campaign.keywords.join(",")}</td>
+                          <td>{campaign.bidAmount}</td>
+                          <td>{campaign.campaignFund}</td>
+                          <td>{campaign.status}</td>
+                          <td>{campaign.town}</td>
+                          <td>{campaign.radius}</td>
+                          <td className="actions-column">
+                              <button onClick={()=>handleEdit(campaign)}>Edit</button>
+                              <button onClick={()=>deleteRecord(campaign.id)}>Delete</button>
+                          </td>
+                      </tr>
+                  ))}
+                  </tbody>
 
 
-        </table>
+              </table>
+          </div>
+          <button className="add-record-button" onClick={()=>setAdding(!adding)}>Add</button>
+      </div>
 
-
-    </div>
   )
 }
 
